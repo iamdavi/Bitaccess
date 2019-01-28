eec23dcf8abc3a427898c503b383baaabaa4acea
txema <txedav@gmail.com>
2019-01-28 23:19:06 +0100

    añadidas urls como numero

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Zona, Puerto
 from django.views.generic.detail import DetailView
+
 
 # Create your views here.
 
@@ -17,6 +18,11 @@ class ZonaDetailView(generic.DetailView):
     Clase para mostrar la informazión de una zona (puertos,descripcion...)
     """
     model = Zona
+
+    def get_object(self, queryset=None):
+        zona = get_object_or_404(Zona, num_zona=self.kwargs['numero'])
+        self.kwargs['pk'] = zona.ip
+        return super(ZonaDetailView, self).get_object(queryset=queryset)
 
     def post(self, request, *args, **kwargs):
         if request.is_ajax():
