db9d78a7a54aaf2e25dc8993bb7946b08fd04100
IamDavid <itsdavid.otero@gmail.com>
2019-02-08 09:48:53 +0100

    Añadidos comentarios y mensajes en los templates

--- catalog/views.py
+++ catalog/views.py
@@ -21,8 +21,8 @@ class ZonaListView(LoginRequiredMixin, generic.ListView):
         """
         Método usado para saber el estado de la zona
         """
-        empleado = Empleado.objects.get(user=self.request.user)
-        zonas = get_zonas_user(self.request.user)
+        empleado = Empleado.objects.get(user=self.request.user) # Obtener usuario logeado
+        zonas = get_zonas_user(self.request.user) # Obtener zonas de la class ConjuntoZona
         zonas = zonas.distinct() # Eliminar los objetos duplicado
         return zonas
 
@@ -35,37 +35,48 @@ class ZonaDetailView(LoginRequiredMixin, generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         grupos = Empleado.objects.get(user=self.request.user).grupo.all() 
+        # Obtener los conjuntos a los que tiene acceso todos los grupos del usuario
         conjuntos = ConjuntoPuerto.objects.filter(grupos_con_acceso__in=grupos)
         puertos = Puerto.objects.none()
+
         for conjunto in conjuntos:
             puertos |= conjunto.puertos.filter(zona=self.object)
+            # Extraer todos los puertos a los que se tiene permiso en la zona 
         puertos = puertos.distinct()
         context['puertos'] = puertos
         return context
 
     def post(self, request, *args, **kwargs):
         if request.is_ajax():
+            # Recogida de datos enviados por Ajax en el template
             id_zona = request.POST['aulaId']
             num_puerto = request.POST['numPuerto']
             estado = request.POST['estado']
+            # Extraer el puerto del que se quiere cambiar el estado
             puerto = Puerto.objects.get(zona__ip=id_zona, num_puerto=num_puerto)
             puerto.encendido = int(estado)
             puerto.save()
 
-            address= ( id_zona, 5001) #define server IP and port
-            client_socket = socket(AF_INET, SOCK_DGRAM) #Set up the Socket
-            client_socket.settimeout(1) #Only wait 1 second for a response
+            #Envio de la orden de cambio al arduino
+            address= ( id_zona, 5001) # Definimos la IP del arduino y del puerto
+            client_socket = socket(AF_INET, SOCK_DGRAM) # Preparamos el socket
+            client_socket.settimeout(1) # Only wait 1 second for a response
 
             if estado == '1':
                 valor_arduino = num_puerto + "on"
+                # mensaje que va a ser leido por el arduino
             else:
                 valor_arduino = num_puerto + "off"
 
-            client_socket.sendto(valor_arduino.encode(), address) #Send the data request
+            client_socket.sendto(valor_arduino.encode(), address) # Enviar orden al arduino
 
             return JsonResponse({})
 
 def get_zonas_user(user):
+    """
+    Método para extraer las zonas a las que tiene acceso
+    un usuario mediante la class ConjuntoZona
+    """
     grupos = Empleado.objects.get(user=user).grupo.all()
     zonas = Zona.objects.none()
     for grupo in grupos:
